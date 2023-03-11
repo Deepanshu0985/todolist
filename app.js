51066f26fb2ea1ec38a535fade5d1277312c5016
Deepanshu0985 <deepanshuy098@gmail.com>
2023-03-11 14:27:49 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -133,6 +133,8 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+
